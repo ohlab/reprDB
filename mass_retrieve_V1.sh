e73df8a92980c8353d95af8c0ee5f4619f5f87ca
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-13 20:56:56 -0700

    fix bug

--- mass_retrieve_V1.sh
+++ mass_retrieve_V1.sh
@@ -20,7 +20,6 @@
 
 infile=$1
 outdir=`dirname $infile`
-cd $outdir
 
 echo 
 echo "This script is for the download of genomes by GenBank accession. Please see Version 2 for download by organism name."
@@ -28,7 +27,7 @@ echo
 
 # Check if the user input is a valid file name
 if [ -f "$infile" ]; then 
-	head -1 ${infile} > heads.txt
+	head -1 ${infile} > ${outdir}/heads.txt
 elif [ "$infile" == "" ]; then
 	echo "No input file provided. Please type the name of the input file after your call to this script."
 	exit
@@ -46,25 +45,25 @@ do
 	if [ $INPUT == 1 ]; then
 		LOOP1=false
 		# Retrieve the GenBank accessions from the PATRIC table
-		colnum=`tr '\t' '\n' < heads.txt | nl | grep 'GenBank Accessions$' | cut -f 1`
-		cut -f ${colnum} ${infile} | sed -e '/^$/d' -e '1d' > GenBankAcc.txt
+		colnum=`tr '\t' '\n' < ${outdir}/heads.txt | nl | grep 'GenBank Accessions$' | cut -f 1`
+		cut -f ${colnum} ${infile} | sed -e '/^$/d' -e '1d' > ${outdir}/GenBankAcc.txt
 	elif [ $INPUT == 2 ]; then
 		LOOP1=false
 		# Retrieve the GenBank accessions from the NCBI table
-		cut -f 1 ${infile} | sed -e '1,2d' -e 's/,.*//' | sort -u -k1,1 > NCBI_acc.txt
-		printf "" > GenBankAcc.txt
-		for NCBI_acc in `cat NCBI_acc.txt`
+		cut -f 1 ${infile} | sed -e '1,2d' -e 's/,.*//' | sort -u -k1,1 > ${outdir}/NCBI_acc.txt
+		printf "" > ${outdir}/GenBankAcc.txt
+		for NCBI_acc in `cat ${outdir}/NCBI_acc.txt`
 		do
-			grep $NCBI_acc $infile | cut -f 2 | tr "\n" "," | sed '$ s/.$//' > GenBank.txt
-			GEN_ACC=`cat GenBank.txt`
-			printf "$GEN_ACC\n" >> GenBankAcc.txt
+			grep $NCBI_acc $infile | cut -f 2 | tr "\n" "," | sed '$ s/.$//' > ${outdir}/GenBank.txt
+			GEN_ACC=`cat ${outdir}/GenBank.txt`
+			printf "$GEN_ACC\n" >> ${outdir}/GenBankAcc.txt
 		done
-		rm GenBank.txt
-		rm NCBI_acc.txt
+		rm ${outdir}/GenBank.txt
+		rm ${outdir}/NCBI_acc.txt
 	elif [ $INPUT == 3 ]; then
 		LOOP1=false
 		# skip to looping through accessions
-		cat $infile > GenBankAcc.txt
+		cat $infile > ${outdir}/GenBankAcc.txt
 	elif [ $INPUT == 4 ]; then
 		printf "\n\nTerminating program...\n\n"
 		exit
@@ -72,25 +71,25 @@ do
 		printf "\n\nInvalid entry. Type a number 1-4.\n"
 	fi
 done
-rm -f heads.txt
+rm -f ${outdir}/heads.txt
 
 printf "\nOne moment please...\n\n"
 
 # Set up the folders and output files
-mkdir _interim
+mkdir ${outdir}/_interim
  
 # Split GenBankAcc.txt into number of specified jobs
-LINES=`wc -l < GenBankAcc.txt |sed 's/^ *//' | sed 's/ .*//'`
-END=`python splitJobs.py GenBankAcc.txt $LINES $JOB_NUM` #generate JOB_NUM of chunks of GenBank accessions, each in a different file
+LINES=`wc -l < ${outdir}/GenBankAcc.txt |sed 's/^ *//' | sed 's/ .*//'`
+END=`python splitJobs.py ${outdir}/GenBankAcc.txt $LINES $JOB_NUM` #generate JOB_NUM of chunks of GenBank accessions, each in a different file
 sleep 3
 printf "Number of jobs generated: $END\n\n"
-rm GenBankAcc.txt
+rm ${outdir}/GenBankAcc.txt
 
 # export ${outdir}
 
 # Job array: for each chunk of accessions, download and edit the .fasta file and output all_lengths.txt
 ########################## mass_retrieve.qsub
-JOB1=`qsub mass_retrieve_V1.qsub -t 1-$END -d $PWD`
+JOB1=`qsub -t 1-$END -d $outdir mass_retrieve_V1.qsub`
 if [ `echo $?` == 0 ]
 then
 	echo $JOB1
